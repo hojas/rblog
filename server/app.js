637c310f549f557e34b1b646472d8189caa169b8
hojas <cyxhuan9@163.com>
2016-11-03 20:25:03 +0800

    remove koa-session2

--- server/app.js
+++ server/app.js
@@ -5,11 +5,9 @@ import send from 'koa-send';
 import bodyParser from 'koa-bodyparser';
 import session from 'koa-session';
 import convert from 'koa-convert';
-//import session from 'koa-session2';
 import mongoose from 'mongoose';
 import views from 'koa-nunjucks-next';
 
-//import Store from './store';
 import routes from './routes';
 
 const app = new Koa();
